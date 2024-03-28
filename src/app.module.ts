6a2679a0313983223c856afdcc44d2a316c16cde
Dominic <kimduumin@gmail.com>
2024-03-28 16:25:22 +0900

    feat. Add react build serving

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { BatchModule } from './batch/batch.module';
 import { ChzzkModule } from './chzzk/chzzk.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [BatchModule, ChzzkModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    BatchModule,
+    ChzzkModule,
+  ],
 })
 export class AppModule {}
