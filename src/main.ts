c36d59be5b8fc68df6e7a023c2248f43afd60f22
doominkim <kimduumin@gmail.com>
2024-06-16 01:49:33 +0900

    fix. cors 허용

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       // Disable error messages in production
