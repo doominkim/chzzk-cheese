fd361dfafe4275b9de062274feb8cb74e1c46f26
doominkim <kimduumin@gmail.com>
2024-06-16 11:38:35 +0900

    fix. add /api to url

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,15 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.setGlobalPrefix('/api');
 
   const config = new DocumentBuilder()
     .setTitle('Chzzk API')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('apis', app, document);
 
-  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       // Disable error messages in production
