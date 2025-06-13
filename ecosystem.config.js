8ddf8af1f993d27d5b7a55f0e4dfb2c29ea30cef
doominkim <kimduumin@gmail.com>
2025-06-13 21:15:07 +0900

    feat(ecosystem): 개발 환경 설정 및 API 배치 추가

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,27 @@
 module.exports = {
   apps: [
     {
-      name: 'chzzk-cheese',
+      name: 'api',
       script: 'npm',
-      args: 'run start:prod',
+      args: 'run start:dev',
       exec_mode: 'fork',
       autorestart: true,
-      watch: true,
+      watch: false,
       env: {
-        NODE_ENV: 'prod',
+        NODE_ENV: 'dev',
+        PORT: 3000,
+      },
+    },
+    {
+      name: 'api-batch',
+      script: 'npm',
+      args: 'run start:dev-batch',
+      exec_mode: 'fork',
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: 'dev',
+        PORT: 3001,
       },
     },
   ],
